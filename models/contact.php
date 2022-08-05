126326707fd5125d6f18810f3412f405a962cbf2
Bletinor <yjtv@protonmail.com>
2022-08-05 15:23:17 -0400

    Update contact.php
    added post function to contact model

--- models/contact.php
+++ models/contact.php
@@ -51,5 +51,36 @@ class Contact
         $this->phoneNum1 = $row['phoneNum1'];
         $this->phoneNum2 = $row['phoneNum2'];
     }
+
+    //post a contact
+    public function post()
+    {
+        $query = 'INSERT INTO ' . $this->table . ' SET name = :name, lastName = :lastName, email = :email, phoneNum1 = :phoneNum1, phoneNum2 = :phoneNum2';
+
+        $statement = $this->conn->prepare($query);
+
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->lastName = htmlspecialchars(strip_tags($this->lastName));
+        $this->email = htmlspecialchars(strip_tags($this->email));
+        $this->phoneNum1 = htmlspecialchars(strip_tags($this->phoneNum1));
+        $this->phoneNum2 = htmlspecialchars(strip_tags($this->phoneNum2));
+
+        $statement->bindParam(':name', $this->name);
+        $statement->bindParam(':lastName', $this->lastName);
+        $statement->bindParam(':email', $this->email);
+        $statement->bindParam(':phoneNum1', $this->phoneNum1);
+        $statement->bindParam(':phoneNum2', $this->phoneNum2);
+
+        if ($statement->execute())
+        {
+            return true;
+        }
+        else
+        {
+            printf($statement->eror);
+
+            return false;
+        }
+    }
 }
  ?>

7a62e2e20f8f1ceb9379808dd88082fb0fbab048
Bletinor <yjtv@protonmail.com>
2022-08-05 10:39:49 -0400

    Create config.php
    added config.php with database parameters and connect function

---
+++ config/config.php
@@ -0,0 +1,28 @@
+<?php
+class Database{
+    //Parameters
+    private $host = 'localhost';
+    private $dbname = 'apidb';
+    private $username = 'root';
+    private $password = 'Oq/E4GEb]LPLu4yQ';
+    private $conn;
+
+    public function connect()
+    {
+        $this->conn = null;
+
+        try
+        {
+            $this->conn = new PDO("mysql:host=" . $this->host . "dbname=" . $this->dbname, $this->username , $this->password , array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn-> setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        }
+        catch (\Throwable $th)
+        {
+            throw $th;
+        }
+
+        return $this->conn;
+    }
+}
+?>

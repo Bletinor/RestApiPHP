3169fe2ea9a132a893109fb88f80b05640e33f60
Bletinor <yjtv@protonmail.com>
2022-08-05 12:43:56 -0400

    Update config.php
    fixed quotes concatenation in host and dbname argumets for PDO

--- config/config.php
+++ config/config.php
@@ -13,7 +13,7 @@ class Database{
 
         try
         {
-            $this->conn = new PDO("mysql:host=" . $this->host . "dbname=" . $this->dbname, $this->username , $this->password , array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->dbname, $this->username , $this->password , array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conn-> setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         }

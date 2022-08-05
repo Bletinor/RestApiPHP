5126b7e89338fec2d7a3164ec11d61b299a40d1e
Bletinor <yjtv@protonmail.com>
2022-08-05 13:28:51 -0400

    Update contact.php
    made get function order by id
    
    created get single contact function

--- models/contact.php
+++ models/contact.php
@@ -22,13 +22,34 @@ class Contact
     //get contacts
     public function get()
     {
-        $query = 'SELECT id, name, lastName, email, phoneNum1, phoneNum2 FROM ' . $this->table . ' ';
+        $query = 'SELECT id, name, lastName, email, phoneNum1, phoneNum2 FROM ' . $this->table . '  ORDER BY id DESC';
 
         $statement = $this->conn->prepare($query);
 
         $statement->execute();
 
         return $statement;
+    }
+
+    //get single contact
+    public function getSingle()
+    {
+        $query = 'SELECT id, name, lastName, email, phoneNum1, phoneNum2 FROM ' . $this->table . ' WHERE id = ? LIMIT 0,1';
+
+        $statement = $this->conn->prepare($query);
+
+        $statement->bindParam(1, $this->id);
+
+        $statement->execute();
+
+        $row = $statement->fetch(PDO::FETCH_ASSOC);
+
+        //set properties
+        $this->name = $row['name'];
+        $this->lastName = $row['lastName'];
+        $this->email = $row['email'];
+        $this->phoneNum1 = $row['phoneNum1'];
+        $this->phoneNum2 = $row['phoneNum2'];
     }
 }
  ?>
